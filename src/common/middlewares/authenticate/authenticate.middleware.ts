89f0cb24f1eeec4887b70ee2ebfe52e23d564b4a
thechotinun <thechotinun@gmail.com>
2024-02-27 17:59:06 +0700

    fix middlewares

--- src/common/middlewares/authenticate/authenticate.middleware.ts
+++ src/common/middlewares/authenticate/authenticate.middleware.ts
@@ -2,20 +2,23 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { AuthException } from '@exceptions/app/auth.exception';
 import * as firebase from 'firebase-admin';
-import * as serviceAccount from './serviceAccount.json';
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.PORT);
 
 const firebase_params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientX509CertUrl: serviceAccount.client_x509_cert_url,
-  universeDomain: serviceAccount.universe_domain,
+  type: process.env.TYPE,
+  projectId: process.env.PROJECT_ID,
+  privateKeyId: process.env.PRIVATE_KEY_ID,
+  privateKey: process.env.PRIVATE_KEY,
+  clientEmail: process.env.CLIENT_EMAIL,
+  clientId: process.env.CLIENT_ID,
+  authUri: process.env.AUTH_URI,
+  tokenUri: process.env.TOKEN_URI,
+  authProviderX509CertUrl: process.env.AUTH_CERT_URL,
+  clientX509CertUrl: process.env.CLIENT_CERT_URL,
+  universeDomain: process.env.UNIVERSAL_DOMAIN,
 };
 
 @Injectable()
